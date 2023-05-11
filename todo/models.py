bde01e85314b1ae1f474d14984d57c9d8673751e
pouya <p.pouya74@gmail.com>
2023-05-11 23:15:57 +0330

    some improvements to models fields

--- todo/models.py
+++ todo/models.py
@@ -1,46 +1,48 @@
 from django.db import models
 
-from accounts.models import User
+from accounts.models import CustomUser
 
-
+STATUS_CHOICES = (
+    (1, 'In Progress'),
+    (2, 'Done'),
+    (3, 'Not Started'),
+    (4, 'Archived'),
+)
 class CommonFieldMixin(models.Model):
-    status_choices = (
-        (1, 'In Progress'),
-        (2, 'Done'),
-        (3, 'Not Started'),
-        (4, 'Archived'),
-    )
     name = models.CharField(max_length=300)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    start_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    start_at = models.DateTimeField(blank=True, null=True)
     deadline = models.DateTimeField(blank=True, null=True)
-    status = models.SmallIntegerField(choices=status_choices)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES)
 
     class Meta:
         abstract = True
+        
+    def get_status_name(self):
+        return self.get_status_display()
 
 class Project(CommonFieldMixin):
-    manager = models.ForeignKey(User, related_name='projects')
+    manager = models.ForeignKey(CustomUser,
+        on_delete=models.SET_NULL,
+        null=True, related_name='projects',)
     
     class Meta:
         db_table = 'projects'
-        ordering = ('-created')
-
+        ordering = ('-created_at',)
+        
     def __str__(self):
-        return f'Project {self.name} created by {self.manager.username} at {self.created}'
+        return self.name
 
 class Task(CommonFieldMixin):
-    developer = models.ForeignKey(User, related_name='tasks') 
+    developers = models.ManyToManyField(CustomUser)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
 
     class Meta:
         db_table = 'tasks'
-        ordering = ('-deadline')
+        ordering = ('-deadline',)
 
     def __str__(self):
-        return f'Task {self.name} created by {self.developer.username} at {self.created}'
-        
-    
+        return self.name
         
