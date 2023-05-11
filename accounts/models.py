2518b5bfe8793b4501fe30cd2abc16ddfa424f63
pouya <p.pouya74@gmail.com>
2023-05-11 23:10:17 +0330

    use AbstractUser and added password hasher

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
 
-
-class User(AbstractBaseUser):
+    
+class CustomUser(AbstractUser):
     ROLES = (
         (1, "Project Manager"),
         (2, "Developer"),
     )
-    role = models.PositiveSmallIntegerField(choices=ROLES)
-    
+    role = models.PositiveSmallIntegerField(choices=ROLES, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(CustomUser, self).save(*args, **kwargs)
