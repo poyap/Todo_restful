0c8f99140ca5744dd87ef3da5845a6af86374573
pouya <p.pouya74@gmail.com>
2023-05-11 23:08:09 +0330

    add custom user serializer

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
-from accounts.models import User
+from accounts.models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = User
-        fields = ['username', 'password', 'role']
+        model = CustomUser
+        fields = [
+            'username',
+            'email',
+            'password',
+            'role',
+        ]
+        extra_kwargs = {'password': {'write_only': True}}
