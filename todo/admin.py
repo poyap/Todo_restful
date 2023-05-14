bba924ef92eaae55a46fb025ed116affb56876a1
pouya <p.pouya74@gmail.com>
2023-05-14 15:49:37 +0330

    add filter_list to models

--- todo/admin.py
+++ todo/admin.py
@@ -3,5 +3,8 @@ from .models import Project, Task
 # Register your models here.
 
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ['manager']
+    list_filter = ['manager']
+admin.site.register(Project, ProjectAdmin, )
 admin.site.register(Task)
